c7f4c96af4584f06106c0396e862cbc993186db2
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-12-17 10:21:23 +0530

    first commit

---
+++ src/navigation/index.tsx
@@ -0,0 +1,20 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
