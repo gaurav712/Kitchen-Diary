df3cb70697b69be65fcd23264dcbb4364788884a
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-12-17 17:04:38 +0530

    add splash screen

--- android/app/src/main/java/com/kitchendiary/MainActivity.java
+++ android/app/src/main/java/com/kitchendiary/MainActivity.java
@@ -1,10 +1,21 @@
 package com.kitchendiary;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
+  /**
+   * Show the splash screen
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import RootStack from './navigation';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <RootStack />
