5f28a920eb3402f2744e03bc906d6f100a2e30a1
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-03-18 20:11:03 +0530

    fix boot splash on android

--- android/app/src/main/java/com/kitchendiary/MainActivity.java
+++ android/app/src/main/java/com/kitchendiary/MainActivity.java
@@ -1,10 +1,11 @@
 package com.kitchendiary;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import org.devio.rn.splashscreen.SplashScreen;
+
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this);
+      RNBootSplash.init(this);
       super.onCreate(savedInstanceState);
   }
 
